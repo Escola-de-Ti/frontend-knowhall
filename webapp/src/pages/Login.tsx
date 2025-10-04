dbe145202ff9b9fc324aa02711acfd2da5365bfb
Kauan Bertalha <kauan.hb2004@gmail.com>
2025-10-03 22:43:09 -0300

    migrando pra form

--- webapp/src/pages/Login.tsx
+++ webapp/src/pages/Login.tsx
@@ -13,16 +13,19 @@ const Login = () => {
       </div>
 
       <div className="login-container">
-        <h1>Bem Vindo!</h1>
-        <div>Nome de Usuário | E-mail</div>
-        <input type="text" />
+        <form action="/login" method="POST">
+          <h1>Bem Vindo!</h1>
 
-        <div>Senha</div>
-        <input type="password" />
+          <label htmlFor="username" className='username'>Nome de Usuário | E-mail</label>
+          <input type="text" name="username" id="username" placeholder="Usuário" required/>
+          
+          <label htmlFor="password" className='password'>Senha</label>
+          <input type="password" name="password" id="password" placeholder="Senha" required/>
 
-        <a href="/">Esqueci minha Senha.</a>
+          <a href="/">Esqueci minha Senha.</a>
 
-        <button type="submit">ENTRAR</button>
+          <input type="submit" className="submit" value="ENTRAR"/>
+        </form>
       </div>
     </>
   );
