2be87564a7020568db2274baafb3fb3b0ba2c1be
Kauan Bertalha <kauan.hb2004@gmail.com>
2025-10-06 22:13:48 -0300

    melhorias tela de login, criado slider, criado pagina de registro apenas para adicionar rota

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import Login from './pages/Login';
+import Cadastro from './pages/Cadastro';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/cadastro" element={<Cadastro />} />
     </Routes>
     </>
   );
