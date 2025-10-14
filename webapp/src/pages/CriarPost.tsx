707dc9be2a4b6e3e4ca1076aaf9728441af27e97
Kauan Bertalha <kauan.hb2004@gmail.com>
2025-10-14 20:22:29 -0300

    melhorias no input imagem

--- webapp/src/pages/CriarPost.tsx
+++ webapp/src/pages/CriarPost.tsx
@@ -7,8 +7,12 @@ const CriarPost = () => {
       <h1>Criar Post</h1>
       <p>Compartilhe seu conhecimento com a comunidade!</p>
 
-      <button type="reset" className='button-cancel'>Cancelar</button>
-      <button type="submit" className='button-post'>Publicar</button>
+      <button type="reset" className="button-cancel">
+        Cancelar
+      </button>
+      <button type="submit" className="button-post">
+        Publicar
+      </button>
 
       <div className="form-section">
         <h2>Conteúdo do Post</h2>
@@ -21,7 +25,24 @@ const CriarPost = () => {
         <p>0/2500 Caracteres</p>
 
         <h3>Imagens (Opcional)</h3>
-        <input className='load-file' type="file" accept="image/*" multiple placeholder='Clique para adicionar imagens ou arraste aqui!'/>
+        <label className="load-file">
+          <svg
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="1.6"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="load-icon"
+          >
+            <path d="M21 15v3a4 4 0 0 1-4 4H7a4 4 0 0 1-4-4v-3" />
+            <path d="M7 10l5-5 5 5" />
+            <path d="M12 5v12" />
+          </svg>
+
+          <p>Clique para adicionar imagens ou arraste aqui</p>
+          <input type="file" accept="image/*" multiple hidden />
+        </label>
       </div>
     </div>
   );
