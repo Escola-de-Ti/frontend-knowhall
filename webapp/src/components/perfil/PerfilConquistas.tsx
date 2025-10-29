4ce84c834a791869306cc260210cedb728480a2c
Kauan Bertalha <kauan.hb2004@gmail.com>
2025-10-28 22:30:06 -0300

    feat: Badges e conquistas inicio

---
+++ webapp/src/components/perfil/PerfilConquistas.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../../styles/perfil/PerfilConquistas.css";
+
+export default function PerfilConquistas() {
+    return (
+        <div className="badges-container">
+            <h2>Badges e Conquistas</h2>
+        </div>
+    );
+};
