0fa50b4ce704aca2f7e10431f51320904b39d4df
Kauan Bertalha <kauan.hb2004@gmail.com>
2025-10-28 22:21:28 -0300

    feat: Slide perfil

--- webapp/src/pages/Perfil.tsx
+++ webapp/src/pages/Perfil.tsx
@@ -1,41 +1,52 @@
-import React from 'react';
-import '../styles/Perfil.css';
-import PerfilDetalhes from '../components/perfil/PerfilDetalhes';
-import PerfilHistorico from '../components/perfil/PerfilHistorico';
+import React, { useState } from 'react'
+import '../styles/Perfil.css'
+import PerfilDetalhes from '../components/perfil/PerfilDetalhes'
+import PerfilHistorico from '../components/perfil/PerfilHistorico'
+import PerfilSlide from '../components/perfil/PerfilSlide'
 
 const Perfil: React.FC = () => {
+  const [aba, setAba] = useState('Conquistas')
+
   return (
     <div className="perfil-page-container">
-      <div className="perfil-grid">
-        <PerfilDetalhes
-          id_usuario={1}
-          email="kauan.hb2004@gmail.com"
-          nome="Kauan Henrique Bertalha"
-          biografia="Desenvolvedor Full Stack com 8 anos de experiência..."
-          id_imagem_perfil=""
-          status_usuario="ATIVO"
-          tipo_usuario="PADRAO"
-          interesses={['MySQL', 'JavaScript']}
-          onEditar={() => {}}
-          onInteresseClick={() => {}}
-        />
+      <div className="perfil-wrap">
+        <div className="perfil-grid">
+          <PerfilDetalhes
+            id_usuario={1}
+            email="kauan.hb2004@gmail.com"
+            nome="Kauan Henrique Bertalha"
+            biografia="Desenvolvedor Full Stack com 8 anos de experiência..."
+            id_imagem_perfil=""
+            status_usuario="ATIVO"
+            tipo_usuario="PADRAO"
+            interesses={['MySQL','JavaScript']}
+            onEditar={()=>{}}
+            onInteresseClick={()=>{}}
+          />
 
-        <PerfilHistorico
-          nivel={15}
-          tokens={5680}
-          ranking={3}
-          xpAtual={27131}
-          xpNecessario={30637}
-          progresso={72}
-          posts={15}
-          upvotes={240}
-          comentarios={47}
-          workshops={8}
-          medalSrc="/medalhaHistorico.png"
+          <PerfilHistorico
+            nivel={15}
+            tokens={5680}
+            ranking={3}
+            xpAtual={27131}
+            xpNecessario={30637}
+            progresso={72}
+            posts={15}
+            upvotes={240}
+            comentarios={47}
+            workshops={8}
+            medalSrc="/medalhaHistorico.png"
+          />
+        </div>
+
+        <PerfilSlide
+          tabs={['Conquistas','Atividades','Certificados','Estatísticas']}
+          value={aba}
+          onChange={setAba}
         />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Perfil;
+export default Perfil
