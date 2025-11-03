7b485808db14d75eae73d94dd6e72fad2d799a0a
Kauan Bertalha <kauan.hb2004@gmail.com>
2025-11-03 12:13:42 -0300

    Delete api.ts

--- webapp/src/services/api.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ?? 'http://localhost:8080',
-  auth: {
-    username: import.meta.env.VITE_API_USER ?? 'user',
-    password: import.meta.env.VITE_API_PASS ?? '',
-  },
-});
